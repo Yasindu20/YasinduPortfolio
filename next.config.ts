b8a85256a153835c22ebf23134c31f8ca0daacdd
Yasindu20 <yasindudemel@gmail.com>
2025-06-07 22:31:13 +0530

    ESLint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Only run ESLint on local development, not during build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
